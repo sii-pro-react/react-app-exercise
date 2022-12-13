5ecc9aa27a3af8c455614210a24b0348f5423170
bbalcar <bbalcar@sii.pl>
2022-12-13 13:31:47 +0100

    Review fixes

--- frontend/src/components/NoData/NoData.tsx
+++ frontend/src/components/NoData/NoData.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Result } from 'antd';
 
-const NoData: React.FC = () => <Result status="error" title="No data to display"></Result>;
+const NoData: React.FC = () => <Result status="error" title="No data to display" />;
 
 export default NoData;
